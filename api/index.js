f9963bd9848e621ae4a0228ce0991eca63b04137
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2023-12-31 20:21:41 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
 dotenv.config();
 
 mongoose
@@ -24,6 +25,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(cors());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
